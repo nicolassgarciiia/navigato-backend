0c872fb0f0fe359b4a209db46a4ac6c9d9bbdd9b
Nicolás García Edo <nicolas27niko@gmail.com>
2025-12-15 10:32:33 +0100

    fix(it02h06): add class-validator decorators to user DTOs to fix login issues

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,8 +1,35 @@
+import { 
+  IsEmail, 
+  IsString, 
+  IsNotEmpty, 
+  MinLength, 
+  IsBoolean, 
+  IsOptional, 
+  MaxLength
+} from 'class-validator';
+
 export class RegisterUserDto {
+  @IsString({ message: 'El nombre debe ser un texto' })
+  @IsNotEmpty({ message: 'El nombre es obligatorio' })
   nombre: string;
+
+  @IsString({ message: 'Los apellidos deben ser un texto' })
+  @IsNotEmpty({ message: 'Los apellidos son obligatorios' })
   apellidos: string;
+
+  @IsEmail({}, { message: 'El formato del correo no es válido' })
+  @IsNotEmpty({ message: 'El correo es obligatorio' })
   correo: string;
+
+  @IsString()
+  @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
+  @MaxLength(16, { message: 'La contraseña puede tener como máximo 16 caracteres'})
   contraseña: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Debes repetir la contraseña' })
   repetirContraseña: string;
+
+  @IsBoolean({ message: 'Debes aceptar la política de privacidad' })
   aceptaPoliticaPrivacidad: boolean;
 }
