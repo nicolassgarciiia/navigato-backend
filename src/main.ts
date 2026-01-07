ee77e14d05a4c9a299788c9f2fff2bb2856355be
JKiddo-dev <matias.aguilar63@gmail.com>
2026-01-07 02:31:46 +0100

    refactor(deploy): refactor for deployment

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "*",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: false,
+    origin: true,
+    methods: "GET,POST,PUT,DELETE,PUT,PATCH,OPTIONS",
+    credentials: true,
   });
 
   app.useGlobalPipes(
@@ -19,6 +19,6 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
